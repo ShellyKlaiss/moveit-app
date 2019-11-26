b3e90157238357254a4dc4c1d604cdfea1382118
Derrick <derrickbusman@gmail.com>
2019-11-25 21:57:22 -0500

    added components

---
+++ src/app/results-info/results-info.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-results-info',
+  templateUrl: './results-info.component.html',
+  styleUrls: ['./results-info.component.css']
+})
+export class ResultsInfoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
