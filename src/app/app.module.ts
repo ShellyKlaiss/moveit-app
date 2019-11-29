bdeee9a91744050d0fc3144eedbb8dccb2d6d041
Derrick <derrickbusman@gmail.com>
2019-11-29 15:10:49 -0500

    second push; added components, api.service.ts, app-routing.module.ts, search-interface.ts, and search.service.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,22 +3,53 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 
+import { MatExpansionModule, MatButtonModule } from '@angular/material';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatSelectModule } from '@angular/material/select';
+import { MatCardModule } from '@angular/material/card';
+import { MatRippleModule } from '@angular/material/core';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatListModule } from '@angular/material/list';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatIconModule } from '@angular/material/icon';
+
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
+import { HomePageComponent } from './home-page/home-page.component';
 import { ResultsComponent } from './results/results.component';
 import { ResultsInfoComponent } from './results-info/results-info.component';
+
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent,
+    HomePageComponent,
     ResultsComponent,
     ResultsInfoComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpClientModule
+    HttpClientModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatExpansionModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatCheckboxModule,
+    MatButtonModule,
+	  MatSelectModule,
+	  MatCardModule,
+	  MatRippleModule,
+	  MatDialogModule,
+	  MatToolbarModule,
+	  MatListModule,
+	  MatRadioModule,
+	  MatIconModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
