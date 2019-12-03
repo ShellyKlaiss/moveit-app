76eac0855f9da8a61a4821074186973bfdc340c5
Derrick <derrickbusman@gmail.com>
2019-12-02 21:53:52 -0500

    Updated Services/API; beginning to create MOGO Pins

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -6,26 +6,26 @@ import { SearchInterface } from './search-interface';
   providedIn: 'root'
 })
 export class ApiService {
-  apikey: string = "30748527d5cb18fb40c29a614c16c1d1";
-  appid: string = "f525747a";
-  apiUrl: string = "https://api.edamam.com/search";
+  // apikey: string = "30748527d5cb18fb40c29a614c16c1d1";
+  // appid: string = "f525747a";
+  apiUrl: string = "https://services2.arcgis.com/qvkbeam7Wirps6zC/arcgis/rest/services/MoGo_Bike_Share_Locations/FeatureServer/0/query?where=1%3D1&outFields=*&outSR=4326&f=json";
 
   constructor(private http: HttpClient) { }
 
   getData(options: SearchInterface) {
     
-    let searchUrl = this.apiUrl + `?q=${options.toggleLocation}?q=${options.customLocation}&app_id=${this.appid}&app_key=${this.apikey}&to=24`;
+    // let searchUrl = this.apiUrl + `?q=${options.toggleLocation}?q=${options.customLocation}&app_id=${this.appid}&app_key=${this.apikey}&to=24`;
 
     
-    if (options.toggleLocation) {
-      searchUrl += `&toggledLocation=${options.toggleLocation}`;
-    }
+    // if (options.toggleLocation) {
+    //   searchUrl += `&toggledLocation=${options.toggleLocation}`;
+    // }
 
    
-	  if (options.customLocation) {
-       searchUrl += `&customLocation=${options.customLocation}`;
-	  }
+	  // if (options.customLocation) {
+    //    searchUrl += `&customLocation=${options.customLocation}`;
+	  // }
 
-    return this.http.get(searchUrl);
+    // return this.http.get(searchUrl);
   }
 }
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 
+declare var H: any;
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -8,6 +10,12 @@ import { Router } from '@angular/router';
 })
 export class AppComponent {
   title = 'moveIt-app';
-	
-	constructor(public router: Router){}
+
+  constructor(public router: Router){
+
+  }
+  
+ 
+
 }
+
